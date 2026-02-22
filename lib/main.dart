a2f67b9f85da4b2c597aad07c40d352f716e433e
Hadiyatou <hadiyaba036@gmail.com>
2026-02-22 03:01:43 +0000

    feat : à modifier

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,32 @@ import 'package:sunu_task/core/theme/app_theme.dart';
 import 'package:sunu_task/screens/splash/splash_screen.dart';
 import 'package:sunu_task/services/storage_service.dart';
 
+
+import 'package:sunu_task/providers/app_provider.dart';
+import 'package:sunu_task/providers/auth_provider.dart';
+import 'package:sunu_task/providers/project_provider.dart';
+import 'package:sunu_task/providers/task_provider.dart';
+// Importe aussi tes futurs écrans
+import 'package:sunu_task/screens/onboarding/onboarding_screen.dart';
+import 'package:sunu_task/screens/auth/login_screen.dart';
+import 'package:sunu_task/screens/home/home_screen.dart';
+
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await StorageService.instance.init();
 
-  runApp(const SunuTask());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AppProvider()..init()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()..init()),
+        ChangeNotifierProvider(create: (_) => ProjectProvider()),
+        ChangeNotifierProvider(create: (_) => TaskProvider()),
+      ],
+      child: const SunuTask(),
+    ),
+  );
 }
 
 class SunuTask extends StatelessWidget {
@@ -18,12 +38,28 @@ class SunuTask extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'SunuTask',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.light,
-      home: SplashScreen(),
+      // Le Consumer écoute les changements dans AppProvider
+      home: Consumer<AppProvider>(
+        builder: (context, app, _) {
+          // 1. Si pas initialisé -> Splash
+          if (!app.isInitialized) return const SplashScreen();
+
+          // 2. Si onboarding pas fait -> Onboarding
+          if (!app.isOnboardingComplete) return const OnboardingScreen();
+
+          // 3. On regarde si l'utilisateur est connecté via AuthProvider
+          return Consumer<AuthProvider>(
+            builder: (context, auth, _) {
+              return auth.isAuthenticated
+                  ? const HomeScreen()
+                  : const LoginScreen();
+            },
+          );
+        },
+      ),
     );
   }
 }
